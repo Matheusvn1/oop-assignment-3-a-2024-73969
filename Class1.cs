abac90a930098c23592574b5d582fe47192901fa
Matheusvn1 <matheusvb.c12@gmail.com>
2024-12-02 15:12:57 +0000

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Banking.Domain
+{
+    public class BankingDriver
+    {
+        public static void Run() {
+            Console.WriteLine("Banking application has started");
+        }
+
+    }
+}
